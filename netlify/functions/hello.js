02e5799468c4bb8e22bbcf55003c58628648f4ed
alex <statefan0826@gmail.com>
2022-05-04 16:53:18 -0400

    t

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,6 +1,7 @@
 exports.handler = async function (event, context) {
     return {
       statusCode: 200,
-      body: "Hello World",
+      body: "Hello World!!!!",
+      locked: false
     };
   }
