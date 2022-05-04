5bfa4a7386e4009f787016732c149a9039b0f034
alex <statefan0826@gmail.com>
2022-05-04 16:57:08 -0400

    testting

--- netlify/functions/hello.js
+++ netlify/functions/hello.js
@@ -1,7 +1,9 @@
 exports.handler = async function (event, context) {
     return {
-      statusCode: 200,
-      body: "Hello World!!!!",
-      locked: false
+        statusCode: 200,
+        body: "Hello World!!!!",
+        locked: false,
+        test: "yes",
+        alsotest: process.env.MONGODB_URI
     };
-  }
+}
