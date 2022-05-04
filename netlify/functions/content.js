a25b89f8747b512d501f5a5f301c8377ade78e67
alex <statefan0826@gmail.com>
2022-05-04 17:05:17 -0400

    iim just silly

--- netlify/functions/content.js
+++ netlify/functions/content.js
@@ -5,7 +5,7 @@ const DB_NAME = 'WEBSITE';
 
 let cachedDb = null;
 
-const connectToDatabase = async () => {
+const connectToDatabase = async (uri) => {
     if(cachedDb) return cachedDb;
 
     const client = await MongoClient.connect(uri, {
@@ -29,7 +29,7 @@ const queryDatabase = async (db) => {
 
 module.exports.handler = async(event, context) => {
     context.callbackWaitsForEmptyEventLoop = false;
-
+    // console.log("MONGODB_URI = " + MONGODB_URI);
     const db = await connectToDatabase(MONGODB_URI);
     return queryDatabase(db);
 };
