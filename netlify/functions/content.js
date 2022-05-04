53b347752837a7bcedc29d17b20fa115ce1115ff
alex <statefan0826@gmail.com>
2022-05-04 18:43:11 -0400

    hhhh

--- netlify/functions/content.js
+++ netlify/functions/content.js
@@ -25,6 +25,7 @@ const queryDatabase = async (db) => {
     const content = await db.collection("content").find({}).toArray();
     console.log("content collection::");
     console.log(db.collection("content"));
+    console.log(await db.collection("content").find({}));
     console.log("content object created");
     console.log("contents of CONTENT");
     console.log(content);
