01e6f7ef9686488609471ddaf17e186a5862ea5d
alex <statefan0826@gmail.com>
2022-06-09 15:17:15 -0400

    increased some scaling

--- public/js/main.js
+++ public/js/main.js
@@ -176,7 +176,7 @@ function window_onMove(event) {
                 var cardWidth = card.offsetWidth, cardHeight = card.offsetHeight;
                 var x = mouseX - offX - cardWidth * 0.5, y = mouseY - offY - cardHeight * 0.5;
                 var rotX = -1 * (x / (cardWidth * 0.5)), rotY = -1 * (y / (cardHeight * 0.5));
-                card.style.transform = "rotate3d(" + rotY + ", " + rotX + ", 0, 5deg) scale(1.2)";
+                card.style.transform = "rotate3d(" + 3 * rotY + ", " + 3 * rotX + ", 0, 5deg) scale(1.2)";
                 cards[i].classList.add("hover");
                 cards[i].classList.remove("unhover");
             }
